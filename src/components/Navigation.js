d29789471b59bc3783819fff256691589397154f
mlazzell17@gmail.com <MicheSi>
2019-12-11 16:45:40 -0700

    working on nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,12 @@ import React from "react";
 export default function NavBar (props) {
 
     return (
-        
+        <nav>
+            <img></img>
+            <a href="#">Home</a>
+            <a href="#">Photo of the Day</a>
+            <a href="#">Previous Photos</a>
+            <a href="#">NASA</a>
+        </nav>
     )
 }
