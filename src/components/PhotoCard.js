f1caaf3d2bff60eddb298a4123a7a352389db66c
mlazzell17@gmail.com <MicheSi>
2019-12-11 14:26:45 -0700

    trying to get axios to work

---
+++ src/components/PhotoCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import axios from "axios";
+import PhotoData from "./PhotoData";
+
+
+export default function PhotoCard () {
+    const [picture, getPicture] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://api.nasa.gov/planetary/apod?api_key=ZwWLfujY6OMgGUkls4B1eGj5ACTgiIAAbO9fKkv2')
+        .then(response => {
+            const photo = response.data.hdurl;
+            getPicture(photo);
+        })
+        .catch(err => {
+            console.log('Cannot read data', err);
+        })
+    }, []);
+}
