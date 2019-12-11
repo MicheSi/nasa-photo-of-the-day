9175dd5774c8378a4f004de019b96a1d7af3c609
mlazzell17@gmail.com <MicheSi>
2019-12-11 16:15:19 -0700

    got info to render to DOM

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
 import "./App.css";
 // components
-import PhotoData from "./components/PhotoData";
 import PhotoCard from "./components/PhotoCard";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-      <PhotoData />
+      <PhotoCard />
     </div>
   );
 }
--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import PhotoData from "./PhotoData";
 
 
 export default function PhotoCard (props) {
@@ -9,8 +8,7 @@ export default function PhotoCard (props) {
     useEffect(() => {
         axios.get('https://api.nasa.gov/planetary/apod?api_key=ZwWLfujY6OMgGUkls4B1eGj5ACTgiIAAbO9fKkv2')
         .then(response => {
-            const photo = response.data.hdurl;
-            getPicture(photo);
+            getPicture(response.data);
         })
         .catch(err => {
             console.log('Cannot read data', err);
@@ -19,10 +17,10 @@ export default function PhotoCard (props) {
 
     return (
         <div className="photoContainer">
-            <h1>Date</h1>
-            <img />
-            
-
+            <h1>{picture.date}</h1>
+            <img className="image" src={picture.url} alt="Nasa Space Photo of the Day" />
+            <h3 className="title">{picture.title}</h3>
+            <p className="description">{picture.explanation}</p>
         </div>
     )
 }
